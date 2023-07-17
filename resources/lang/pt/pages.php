e62be207417e5caefe83ffd7f0aeeb4a0d567967
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-07-17 13:34:49 +0100

    about me page translation to portuguese

---
+++ resources/lang/pt/pages.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'about-me' => [
+        'about_me' => 'Sobre mim',
+        'about_me_text' => 'Eu sou, em primeiro lugar, um ser humano, um amigo e um eterno estudante da vida.',
+
+        'my_interest_in_tech' => 'Meu interesse por tecnologia',
+        'my_interest_in_tech_txt_1' => 'Em algum momento, bem cedo na minha vida, por volta dos 12 anos, tive meu primeiro e tão desejado computador, até então, só tinha visto meus amigos usarem tal máquina, e às vezes tinha o privilégio de jogar com eles em seus computadores.',
+        'my_interest_in_tech_txt_2' => 'Desde o primeiro dia que recebi meu primeiro computador, o interesse foi grande, e logo fui explorando as configurações do computador, jogos offline locais e qualquer coisa que eu pudesse clicar... e claro que não demorou muito até eu descobrir a internet!',
+
+        'evolving_with_the_internet' => 'Evoluindo com a Internet',
+        'evolving_with_the_internet_txt_1' => 'Desde que descobri a internet, comecei a me aprofundar nos mundos da informação.',
+        'evolving_with_the_internet_txt_2' => 'Logo descobri que gostava de ler e aprender e também de criar e compartilhar conteúdo digital.',
+        'evolving_with_the_internet_txt_3' => 'Só Deus sabe quanta informação/software eu explorei desde que descobri a internet!',
+
+        'becoming_a_pro' => 'O caminho para o profissionalismo',
+        'becoming_a_pro_txt_1' => 'Não apenas levei inúmeras horas explorando assuntos de forma livre e aleatória em minha máquina, mas também passei por 2 cursos diferentes de TI durante minha formação acadêmica.',
+        'becoming_a_pro_txt_2' => 'Um deles - Instalação e Operação de Sistemas Informáticos - com a duração de 2 anos, e outro - Gestão e Programação de Sistemas Informáticos - com a duração de 3 anos.',
+        'becoming_a_pro_txt_3' => 'Comecei a trabalhar oficialmente logo após terminar o segundo curso, e tenho trabalhado para diferentes empresas desde então. Tenho mais de 10 anos de experiência como desenvolvedor web!',
+
+        'other_interests' => 'Outros interesses',
+        'other_interests_txt_1' => 'Fora do mundo de TI, tenho uma profunda atração por esportes e natureza.',
+        'other_interests_txt_2' => 'Um dos meus esportes favoritos é a Calistenia / Street Workout, que é basicamente ginástica / treinar com o peso do próprio corpo.',
+        'other_interests_txt_3' => 'Pode ver abaixo alguns exercícios de peso corporal "especiais" que tenho guardados na minha mochila',
+        'other_interests_txt_4' => '( a propósito, eu acelerei o vídeo )',
+        'other_interests_txt_5' => 'Também tive o privilégio de participar/praticar Artes Marciais ao longo da minha vida, um pouco de kickboxing, um pouco de jiu jitsu, um pouco de capoeira... e adoro assistir UFC!',
+        'other_interests_txt_6' => 'Eu gosto de rodas...eu não tenho ainda a minha carta de condução, mas eu gosto de andar de BMX ou de skate às vezes.. Patins também são divertidos! Eu também tive o privilégio de esquiar no passado e foi uma experiência e tanto!',
+        'other_interests_txt_7' => 'aqui está um vídeo meu patinando / pedalando no meu skate:',
+        'other_interests_txt_8' => 'Também adoro o mar, principalmente quando tem boas ondas para apanhar! Não tenho prancha de surf mas posso apanhá-las com o meu próprio corpo!',
+        'other_interests_txt_9' => 'Bem, isso é apenas a ponta do iceberg, há um monte de coisas nesta vida que eu gosto, não vale a pena mencionar todas elas. Vou continuar vivendo e tentando o meu melhor para aproveitar a vida o máximo que puder da melhor maneira possível! - e - espero que você possa fazer isso também!!',
+    ]
+];
