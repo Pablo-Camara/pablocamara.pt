13670ddf4e550fce8d3d463fb92558ea321521f5
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-07-18 13:39:49 +0100

    call back for after menu animation

--- public/assets/js/main-menu.js
+++ public/assets/js/main-menu.js
@@ -1,6 +1,14 @@
-function animateMainMenuItems(visible) {
+function animateMainMenuItems(visible, callbackAfter) {
     const mainMenu = document.getElementById('main-menu');
     const mainMenuItems = mainMenu.getElementsByTagName('a');
+    const msTimeInBetween = 800;
+    if (typeof callbackAfter === 'function') {
+        setTimeout (function() {
+            callbackAfter();
+        }, (mainMenuItems.length+1) * msTimeInBetween);
+    }
+
+
     for (var i = 1; i <= mainMenuItems.length; i++) {
         const mainMenuItem = mainMenuItems[i-1];
 
@@ -10,6 +18,6 @@ function animateMainMenuItems(visible) {
 
         setTimeout (function() {
             mainMenuItem.style.display = visible ? 'block' : 'none';
-        }, i * 800);
+        }, i * msTimeInBetween);
     }
 }
