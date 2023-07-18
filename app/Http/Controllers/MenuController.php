eb93f28098c1f66dcdd95503ce72abbd24b17c3e
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-07-18 18:31:08 +0100

    properly respond with cookie has-animated-main-menu-once

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Cookie;
 
 class MenuController extends Controller
 {
-    public function hasAnimatedMainMenuOnce (Request $request) {
-        $threeDaysInMinutes = (24*60)*3;
-        Cookie::queue('has-animated-main-menu-once', true, $threeDaysInMinutes);
+    public function hasAnimatedMainMenuOnce(Request $request)
+    {
+        $threeDaysInMinutes = (24 * 60) * 3;
+        $cookie = cookie('has-animated-main-menu-once', true, $threeDaysInMinutes);
+
+        $response = new Response('has-animated-main-menu-once-cookie-set');
+        $response->withCookie($cookie);
+
+        return $response;
     }
 }
