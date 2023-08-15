c2b287c8e10fb4692ebfc796a17f22f7866f1fdc
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-08-15 15:55:00 +0100

    reduced pictures

--- app/View/Composers/AboutMeComposer.php
+++ app/View/Composers/AboutMeComposer.php
@@ -15,30 +15,7 @@ class AboutMeComposer
         $profilePictures = [
             'assets/img/profile-picture-1.jpeg',
             'assets/img/profile-picture-26.jpg',
-            'assets/img/profile-picture-2.jpg',
-            'assets/img/profile-picture-3.jpg',
-            'assets/img/profile-picture-4.jpg',
-            'assets/img/profile-picture-5.jpg',
-            'assets/img/profile-picture-6.jpg',
-            'assets/img/profile-picture-7.jpg',
-            'assets/img/profile-picture-8.jpg',
-            'assets/img/profile-picture-9.jpg',
-            'assets/img/profile-picture-10.jpg',
-            'assets/img/profile-picture-11.jpg',
-            'assets/img/profile-picture-12.jpg',
-            'assets/img/profile-picture-13.jpg',
-            'assets/img/profile-picture-14.jpg',
-            'assets/img/profile-picture-15.jpg',
-            'assets/img/profile-picture-16.jpg',
-            'assets/img/profile-picture-17.jpg',
-            'assets/img/profile-picture-18.jpg',
-            'assets/img/profile-picture-19.jpg',
-            'assets/img/profile-picture-20.jpg',
-            'assets/img/profile-picture-21.jpg',
-            'assets/img/profile-picture-22.jpg',
-            'assets/img/profile-picture-23.jpg',
-            'assets/img/profile-picture-24.jpg',
-            'assets/img/profile-picture-25.jpg',
+            'assets/img/profile-picture-2.jpg'
         ];
 
         for($i = 0; $i < count($profilePictures); $i++) {
